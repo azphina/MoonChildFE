f69aadc945d898ce410bb28c4fac9ad701057936
Mors <morsgames8@gmail.com>
2026-05-09 02:58:18 +0300

    Add windows metadata

--- CMake/Platforms/Windows.cmake
+++ CMake/Platforms/Windows.cmake
@@ -1,3 +1,9 @@
+configure_file(
+    "${CMAKE_SOURCE_DIR}/MoonChild.rc.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/MoonChild.rc"
+    @ONLY
+)
+
 target_compile_definitions(${EXECUTABLE_NAME} PRIVATE NOMINMAX)
 
-target_sources(${EXECUTABLE_NAME} PRIVATE "${CMAKE_SOURCE_DIR}/MoonChild.rc")
+target_sources(${EXECUTABLE_NAME} PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/MoonChild.rc")
